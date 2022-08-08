f5110df980ea02f4fc87cba4b3e260dfda5e7311
EduardoGomes1982 <edu1982talk@gmail.com>
2022-08-07 21:43:54 -0300

    Desafio Concluído

---
+++ backend/src/main/java/com/devsuperior/evento/DesafioEventoApplication.java
@@ -0,0 +1,12 @@
+package com.devsuperior.evento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioEventoApplication.class, args);
+	}
+}
